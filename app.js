ebb53cefe8088fd7b1dea6542608280417119436
arshdeepdgreat <arshdeepdgreat@gmail.com>
2022-02-05 14:10:21 +0530

    signup page

--- app.js
+++ app.js
@@ -6,7 +6,11 @@ const bodyParser = require("body-parser");
 const request = require("request");
 
 const app = express();
+app.use(express.static("public"));
 
+app.get("/",function(req,res){
+    res.sendFile(__dirname+"/signup.html");
+})
 app.listen(3000,function(){
     console.log("server running on port 3000")
 })
